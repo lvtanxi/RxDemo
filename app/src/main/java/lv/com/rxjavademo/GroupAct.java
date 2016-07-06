275038bd2bccc420c9d604c2aebd9cd5fdf028f0
lvyong <lvyong@retail-tek.com>
2016-07-06 10:27:51 +0800

    完善demo

--- app/src/main/java/lv/com/rxjavademo/GroupAct.java
+++ app/src/main/java/lv/com/rxjavademo/GroupAct.java
@@ -8,9 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.functions.Func2;
 
 /**
  * User: 吕勇
@@ -39,7 +43,7 @@ public class GroupAct extends AppCompatActivity {
     }
 
     public void startWith(View view) {
-        Observable.range(1,10).startWith(0)
+        Observable.range(1, 10).startWith(0)
                 .compose(RxSchedulers.<Integer>io_main())
                 .subscribe(new Action1<Integer>() {
                     @Override
@@ -50,12 +54,203 @@ public class GroupAct extends AppCompatActivity {
     }
 
     public void merge(View view) {
-        Observable.merge(Observable.range(0,2),Observable.just("lv","tanxi"))
+        Observable.merge(Observable.range(0, 2), Observable.just("lv", "tanxi"))
                 .compose(RxSchedulers.<Serializable>io_main())
                 .subscribe(new Action1<Serializable>() {
                     @Override
                     public void call(Serializable serializable) {
                         DLog.d(serializable);
+                    }
+                });
+    }
+
+    public void mergeDelayError(View view) {
+        //产生0,5,10数列,最后会产生一个错误
+        Observable<Long> errorObservable = Observable.error(new Exception("this is end!"));
+        Observable<Long> observable1 = Observable.interval(0, 1000, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong) {
+                        return aLong * 5;
+                    }
+                }).take(3).mergeWith(errorObservable.delay(3500, TimeUnit.MILLISECONDS));
+
+        //产生0,10,20,30,40数列
+        Observable<Long> observable2 = Observable.interval(500, 1000, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong) {
+                        return aLong + 11;
+                    }
+                }).take(5);
+
+        Observable.mergeDelayError(observable1, observable2)
+                .subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onCompleted() {
+                        DLog.d("Sequence complete.");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        DLog.d("Error: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        DLog.d("Next:" + aLong);
+                    }
+                });
+    }
+
+    public void zip(View view) {
+        Observable<Integer> observable1 = Observable.just(10, 20, 30);
+        Observable<Integer> observable2 = Observable.just(4, 8, 12, 16);
+        Observable.zip(observable1, observable2, new Func2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        }).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                DLog.d("Sequence complete.");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                DLog.d("Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                DLog.d("Next:" + value);
+            }
+        });
+    }
+
+    public void switchOnNext(View view) {
+        Observable<Observable<Long>> observable = Observable.interval(0, 500, TimeUnit.MILLISECONDS).map(new Func1<Long, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(Long aLong) {
+                return Observable.interval(0, 200, TimeUnit.MILLISECONDS).map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong) {
+                        return aLong * 10;
+                    }
+                }).take(5);
+            }
+        }).take(2);
+
+        Observable.switchOnNext(observable).subscribe(new Subscriber<Long>() {
+            @Override
+            public void onCompleted() {
+                DLog.d("Sequence complete.");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                DLog.d("Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                DLog.d("Next:" + aLong);
+            }
+        });
+    }
+
+    public void join(View view) {
+        Observable.just(1).join(Observable.just(2), new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                return Observable.just(integer).delay(100, TimeUnit.MILLISECONDS);
+            }
+        }, new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                return Observable.just(integer).delay(2000, TimeUnit.SECONDS);
+            }
+        }, new Func2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        }).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                DLog.d(integer);
+            }
+        });
+
+
+
+
+
+        Observable.just(1L,2L).groupJoin(Observable.just(3L,4L), new Func1<Long, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(Long aLong) {
+                return Observable.just(aLong).delay(1600, TimeUnit.MILLISECONDS);
+            }
+        }, new Func1<Long, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(Long aLong) {
+                return Observable.just(aLong).delay(600, TimeUnit.MILLISECONDS);
+            }
+        }, new Func2<Long, Observable<Long>, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(final Long aLong, Observable<Long> observable) {
+                return observable.map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong2) {
+                        return aLong + aLong2;
+                    }
+                });
+            }
+        }).subscribe(new Subscriber<Observable<Long>>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("Sequence complete.");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(Observable<Long> observable) {
+                observable.subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                      DLog.d("Next: " + aLong);
+                    }
+                });
+            }
+        });
+    }
+
+    public void combineLatest(View view) {//有个问题就是第一个Observable只显示了最后一个
+        Observable.combineLatest(Observable.just(1,2), Observable.just("sda", "sadf"), new Func2<Integer, String, String>() {
+            @Override
+            public String call(Integer integer, String s) {
+                return integer+s;
+            }
+        }).compose(RxSchedulers.<String>io_main())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        DLog.d(s);
                     }
                 });
     }
